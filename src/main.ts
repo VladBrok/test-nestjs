fd163ea69d355f77a3dcf3291797aea3e272b043
Vlad Brok <vlad.brok99@gmail.com>
2022-10-02 22:02:55 +0300

    Change npm start command

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { logger } from './logger.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.use(logger);
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
